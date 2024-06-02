9302e60a46b0da65f30654671e084bdf7f823650
rakitenkogermans <rakitenko.germans@gmail.com>
2024-06-02 17:07:04 +0300

    linked stripe customer

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,23 +1,32 @@
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts';
 import { stripe } from '../_utils/stripe.ts';
+import {createOrRetrieveProfile} from "../_utils/supabase.ts";
 
 console.log('Hello from Functions!');
 
-serve(async (req) => {
+serve(async (req: Request) => {
   try {
     const { amount } = await req.json();
 
+    const customer = await createOrRetrieveProfile(req)
     // Create a PaymentIntent so that the SDK can charge the logged in customer.
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer },
+        { apiVersion: "2020-08-27" }
+    );
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: amount,
       currency: 'usd',
-      // customer: customer,
+      customer: customer,
+      // ephemeralKey: ephemeralKey.secret,
     });
+
     const res = {
       publishableKey: Deno.env.get('EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY'),
       paymentIntent: paymentIntent.client_secret,
-      // ephemeralKey: ephemeralKey.secret,
-      // customer: customer,
+      ephemeralKey: ephemeralKey.secret,
+      customer: customer,
     };
     return new Response(JSON.stringify(res), {
       headers: { 'Content-Type': 'application/json' },
